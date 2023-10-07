4ac08d57840cd12246fd7497ef7049a751c36d34
AdithyanA <adithyanayithala@gmail.com>
2023-10-07 19:39:03 +0530

    fixed chat

--- finfrontend/src/pages/dashboard/chat_bubble.jsx
+++ finfrontend/src/pages/dashboard/chat_bubble.jsx
@@ -1,16 +1,13 @@
-export default function ChatBubble({sent , message}){
-    var sender;
-    if(sent === true){
-        sender = "chat-end"
-    }
-    else{
-        sender = "chat-start"
-    }
-    return(
-        <div className={sender}>
-            <div className="chat-bubble">
-                {message}
-            </div>
-        </div>
-    );
+export default function ChatBubble({ sent, message }) {
+  var sender;
+  if (sent === true) {
+    sender = "chat-end self-end py-5";
+  } else {
+    sender = "chat-start py-5";
+  }
+  return (
+    <div className={sender}>
+      <div className="chat-bubble max-w-xs">{message}</div>
+    </div>
+  );
 }
--- finfrontend/src/pages/dashboard/dashboard.jsx
+++ finfrontend/src/pages/dashboard/dashboard.jsx
@@ -1,9 +1,9 @@
 import ChatBubble from "./chat_bubble.jsx";
-import './dashboard.css'
+import "./dashboard.css";
 export default function Dashboard() {
   return (
     <div className="dashboard-container text-[black] flex h-screen box-border">
-      <div className="box-border flex-none w-20 rounded-md bg-slate-500 m-3 fixed h-full">
+      <div className="box-border flex-none w-20 rounded-md bg-slate-500 m-3 fixed h-[680px]">
         <div className="avatar">
           <div className="w-16 p-2 mt-2">
             <img src="https://picsum.photos/200/300" className="rounded-md" />
@@ -11,12 +11,17 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="flex-1 w-1/4">content</div>
-      <div className="flex-none flex flex-col-reverse w-96 fixed right-0 bg-slate-500 rounded-md h-full m-3 box-border ">
-          <input type="text" className="chat-input input m-6" />
-          <div className="flex flex-col chat-container mx-8 min-w-96 box-border" >
-              <ChatBubble sent={false} message="hi" className="justify-self-end"/>
-              <ChatBubble sent={true} message="hello" className="align-self: flex-end margin-left: 8px"/>
-          </div>
+      <div className="flex-none flex flex-col-reverse w-96 fixed right-0 bg-slate-500 rounded-md h-[680px] m-3 box-border ">
+        <input
+          type="text"
+          className="chat-input input m-6"
+          placeholder="chat"
+        />
+        <div className="flex flex-col chat-container  min-w-96 px-7 box-border">
+          <ChatBubble sent={false} message="hi" />
+          <ChatBubble sent={true} message="hello" />
+          <ChatBubble sent={true} message="what u doing" />
+        </div>
       </div>
     </div>
   );
