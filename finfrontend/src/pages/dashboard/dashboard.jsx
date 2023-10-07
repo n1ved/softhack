889810ba415571cbf58e26cdb392d86a2d495a3d
n1ved <nivedrsalini@gmail.com>
2023-10-07 18:38:11 +0530

    Changes req fix

--- finfrontend/src/pages/dashboard/dashboard.jsx
+++ finfrontend/src/pages/dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
-    <div className="dashboard-container text-[black] flex h-screen">
-      <div className="flex-none w-20 rounded-md bg-slate-500 m-3 fixed h-[680px]">
+    <div className="dashboard-container text-[black] flex h-screen box-border">
+      <div className="box-border flex-none w-20 rounded-md bg-slate-500 m-3 fixed h-full">
         <div className="avatar">
           <div className="w-16 p-2 mt-2">
             <img src="https://picsum.photos/200/300" className="rounded-md" />
@@ -9,7 +9,7 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="flex-1 w-1/4">content</div>
-      <div className="flex-none w-64 fixed right-0 bg-slate-500 rounded-md h-[680px] m-3">
+      <div className="flex-none w-96 fixed right-0 bg-slate-500 rounded-md h-full m-3 box-border ">
         chat
       </div>
     </div>
