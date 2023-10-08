f47ed96bdca40425f486c007c68a8b01a494b47c
Alex <mathewalex806@gmail.com>
2023-10-08 19:12:14 +0530

    new Company model

--- finbackend/fintech/models.py
+++ finbackend/fintech/models.py
@@ -4,3 +4,8 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     pass
+
+# creating company model
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=28, blank=True)
